005b8f39e8077945aa3a608c33df7edfae4f7dbf
Santiago Lopez <santilm.sjlm@gmail.com>
2024-12-07 17:03:23 -0300

    jugando con fetch y axios

--- part2/2.6/src/App.jsx
+++ part2/2.6/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 /* eslint-disable react/prop-types */
 const Filter = ({ filter, setFilter }) => {
@@ -77,15 +78,17 @@ const Nums = ({ filtered }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((json) => {
+      const { data } = json;
+      setPersons(data);
+    });
+  }, []);
 
   const filtered = filter
     ? persons.filter((person) => person.name.includes(filter))
